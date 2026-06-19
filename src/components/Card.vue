50a26c925613823b2cec5dbafa7cfa46dd5452cb
Kamillaaks <kammi.ar@hotmail.com>
2026-06-19 11:48:55 +0200

    Implement Card component and computed properties

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { CardData } from '../types'
+
+defineProps<{
+    card: CardData
+}>()
+
+const emit = defineEmits<{
+    cardClicked: [cardId: string]
+}>()
+</script>
+
+<template>
+    <div 
+        class="card" 
+        :class="card.state"
+        @click="emit('cardClicked', card.id)"
+        >
+        {{ card.state !== 'hidden' ? card.symbol: '' }}
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 80px;
+    height: 120px;
+    border: 1px solid black;
+}
+</style>

844d1d8d6eaf783643dde99ed75ea7672fee133b
Kamillaaks <kammi.ar@hotmail.com>
2026-06-22 13:45:25 +0200

    Fix Netlify base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/memory-game/',
-
-    plugins: [
-        vue(),
-        vueDevTools(),
-    ],
-    resolve: {
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },

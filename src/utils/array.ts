1ce753fbf9016a670d8b9ed5e734ddae310928f6
Kamillaaks <kammi.ar@hotmail.com>
2026-06-24 11:23:42 +0200

    Remove unnecessary comments

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,15 +3,12 @@ import type { CardSymbol, HighScoreEntry } from "../types";
 export function shuffle(array: CardSymbol[]): void {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    //[array[i], array[j]] = [array[j], array[i]]; Bytter ut fordi TS vil ha det strengt
-
+    
     const temp = array[i]
     if (temp === undefined || array[j] === undefined) {
         continue
     }
-    // Denne sier: Om en av verdiene mangler, hopp over denne runden
-    // Ts blir fornøyd fordi jeg har bevist at verdiene finnes før de byttes
-
+   
     array[i] = array[j]
     array[j] = temp
   }
